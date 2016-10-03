9196b8d7a9c8ac0b324b6437bee3f413e60e7dd3
Alexandre Roch <maycon.alexrocha@gmail.com>
2016-10-02 22:08:19 -0300

    corrigindo problemas

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,5 @@
 const body = document.querySelector('body')
-const h1 = document.querySelector('section h1')
+const h1 = document.querySelector('main section h1')
 
 hora = () => {
   let data = new Date()
@@ -22,7 +22,6 @@ const css = () => {
   sec = sec < 10 ? '0' + sec : sec
   return "#" + hora + "" + min + "" + sec
 }
-
 setInterval(()=> {
   let data = new Date()
   h1.innerHTML = hora()
